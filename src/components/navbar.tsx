e106bcb8b08c2fc20564b4ba971f9e8a042a9e74
renn72 <simplesam8856@gmail.com>
2023-06-13 14:52:49 +1000

    another .current

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -85,10 +85,9 @@ const Navbar = () => {
                     as="a"
                     href={item.href}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      item.href === router.pathname ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'block rounded-md px-3 py-2 text-base font-medium'
                     )}
-                    aria-current={item.current ? 'page' : undefined}
                   >
                     {item.name}
                   </Disclosure.Button>
