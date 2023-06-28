bf682deef15335a5bde0b76bc4e19f70f6824ea4
renn72 <simplesam8856@gmail.com>
2023-06-28 10:22:50 +1000

    the width fight

--- src/pages/templates2/form.tsx
+++ src/pages/templates2/form.tsx
@@ -31,7 +31,9 @@ import {
   AccordionTrigger,
 } from '@/components/ui/accordion'
 
-import { Disclosure, } from '@headlessui/react'
+import {
+  Disclosure, Transition,
+} from '@headlessui/react'
 import { ChevronUpIcon, } from '@heroicons/react/20/solid'
 
 export const formDayAtom = atom(0)
@@ -75,7 +77,7 @@ const dayText = [
 ]
 
 const defaultValues = {
-  name: '',
+  name: `block-${getRandomInt(1000)}`,
   week: [
     {
       day: [
@@ -296,28 +298,29 @@ const Form = () => {
 
   return (
     <>
-      <div className='flex gap-2 sm:gap-6 justify-center items-center text-sm sm:text-base font-semibold'>
-
-        <div>
-          <Button
-            className=''
-          >
-            New Template
-          </Button>
-        </div>
-
-      </div>
-      <div className='mt-2 md:mt-8 text-xxs md:text-sm flex flex-col items-center'>
+      <div className='mt-2 md:mt-8 text-xxs md:text-sm w-full flex flex-col justify-center items-center px-2 '>
         <FormProvider {...formMethods}>
-          <form onSubmit={handleSubmit(onSubmit, onError)}>
-            <div className='flex flex-col gap-1 sm:gap-4 border border-gray-600 rounded-xl p-2 sm:p-6'>
+          <form onSubmit={handleSubmit(onSubmit, onError)} className='w-full flex flex-col justify-center items-center'>
+            <div className='flex flex-col w-full max-w-7xl gap-1 sm:gap-4 border border-gray-600 rounded-xl p-2 sm:p-6'>
+
+              {/* template select */}
+              <div className='flex gap-2 items-center justify-center'>
+                <div>
+                  <Button
+                    type='button'
+                    className=''
+                  >
+                    New Template
+                  </Button>
+                </div>
+              </div>
 
               {/* Title */}
               <div className='flex flex-col gap-2 items-center justify-center'>
                 <div className='relative rounded-md shadow-lg'>
                   <Input className=''
                     placeholder='Title'
-                    defaultValue={`block-${getRandomInt(1000)}`}
+                    defaultValue={``}
                     {...register('name', { required: 'This is required.', })}
                   />
                 </div>
@@ -333,21 +336,30 @@ const Form = () => {
               {/* form */}
               {
                 weekField.fields.map((week, weekIdx) => (
-                  <Disclosure key={week.id} as='div' >
+                  <Disclosure key={week.id} >
                     {({ open, }) => (
-                      <div className='border border-gray-400 p-2 rounded-xl'>
-                        <Disclosure.Button className='flex w-full justify-between rounded-lg px-4 py-2 text-left text-lg hover:bg-gray-200 hover:text-gray-900 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'>
+                      <div className='border border-gray-400 min-w-full p-2 rounded-xl'>
+                        <Disclosure.Button className='flex justify-between rounded-lg px-8 py-2 text-left text-lg hover:bg-gray-200 hover:text-gray-900 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75'>
                           <span>{`Week ${weekIdx + 1}`}</span>
                           <ChevronUpIcon
                             className={`${open ? 'rotate-180 transform' : ''
                               } h-5 w-5 text-gray-400`}
                           />
                         </Disclosure.Button>
-                        <Disclosure.Panel>
-                          <div className={`flex gap-10`}>
-                            <FormWeek weekIdx={weekIdx} />
-                          </div>
-                        </Disclosure.Panel>
+                        <Transition
+                          enter='transition duration-100 ease-out'
+                          enterFrom='transform scale-95 opacity-0'
+                          enterTo='transform scale-100 opacity-100'
+                          leave='transition duration-75 ease-out'
+                          leaveFrom='transform scale-100 opacity-100'
+                          leaveTo='transform scale-95 opacity-0'
+                        >
+                          <Disclosure.Panel>
+                            <div className={`flex gap-10`}>
+                              <FormWeek weekIdx={weekIdx} />
+                            </div>
+                          </Disclosure.Panel>
+                        </Transition>
                       </div>
                     )}
                   </Disclosure>
