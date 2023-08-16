9a227032c1994524ebf3b1236d21405938e0e795
renn72 <simplesam8856@gmail.com>
2023-08-16 15:33:28 +1000

    input coloring

--- @/components/ui/input.tsx
+++ @/components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border focus:bg-gray-700 border-gray-600 bg-gray-900 px-3 py-2 text-sm text-gray-200 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border focus:bg-gray-800 border-gray-700 bg-black px-3 py-2 text-sm text-gray-200 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring focus-visible:ring-ring focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
