a313564e11b18aaa6253518caf6570a49939f620
renn72 <simplesam8856@gmail.com>
2023-08-29 14:24:51 +1000

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { withClerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export default withClerkMiddleware(() => {
-    console.log('in the middle')
     return NextResponse.next();
 });
 
