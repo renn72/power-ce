b51d00738fc5272d65e9ed5bd36dc3a521e5c145
renn72 <simplesam8856@gmail.com>
2023-09-12 11:57:22 +1000

    change countdown to call all users

--- src/components/countDown.tsx
+++ src/components/countDown.tsx
@@ -1,47 +1,36 @@
-import React, {
-  useEffect, useState,
-} from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { api, } from '~/utils/api'
+import { api } from '~/utils/api'
 
 import Countdown from 'react-countdown'
 
-const CountDown = (
-  { userId, }:
-    {
-      userId: string,
-    }
-) => {
-  const [
-    compDate,
-    setCompDate,
-  ] = useState<Date>()
-  const [
-    compName,
-    setCompName,
-  ] = useState<string>('')
-  const { data: compDateUser, } = api.compDate.getAllUser.useQuery({ userId: userId, })
+const CountDown = ({ userId }: { userId: string }) => {
+  const [compDate, setCompDate] = useState<Date>()
+  const [compName, setCompName] = useState<string>('')
+  const { data: compDateAll } = api.compDate.getAll.useQuery()
+
+  const compDateUser = compDateAll?.filter(
+    (compDate) => compDate.userId === userId,
+  )
 
   useEffect(() => {
     if (compDateUser && compDateUser.length > 0 && compDateUser[0]) {
       setCompDate(new Date(compDateUser[0].date))
       setCompName(compDateUser[0].name)
     }
-  }, [compDateUser,])
+  }, [compDateUser])
 
   return (
     <>
-      {compDate && compName
-        && (
-          <div className='flex flex-col md:flex-row md:gap-4 items-center md:items-baseline'>
-            <h2 className='text-lg font-bold text-gray-300'>{compName}</h2>
-            <Countdown
-              className='text-xl font-bold'
-              date={compDate}
-            />
-          </div>
-        )
-      }
+      {compDate && compName && (
+        <div className='flex flex-col items-center md:flex-row md:items-baseline md:gap-4'>
+          <h2 className='text-lg font-bold text-gray-300'>{compName}</h2>
+          <Countdown
+            className='text-xl font-bold'
+            date={compDate}
+          />
+        </div>
+      )}
     </>
   )
 }
