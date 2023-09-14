6d2a0858784f66192e06e630c7cd8392217a958d
renn72 <simplesam8856@gmail.com>
2023-09-14 15:41:31 +1000

    clean userprogram from template select

--- src/pages/users/templateSelect.tsx
+++ src/pages/users/templateSelect.tsx
@@ -1,12 +1,8 @@
-import {
-  useState, useEffect, Fragment,
-} from 'react'
+import { useState, useEffect, Fragment } from 'react'
 
-import { api, } from '~/utils/api'
+import { api } from '~/utils/api'
 
-import {
-  Listbox, Transition,
-} from '@headlessui/react'
+import { Listbox, Transition } from '@headlessui/react'
 import {
   ChevronUpDownIcon,
   CheckIcon,
@@ -15,43 +11,29 @@ import {
   XCircleIcon,
 } from '@heroicons/react/24/outline'
 
-import { capitaliseString, } from '~/utils/utils'
+import { capitaliseString } from '~/utils/utils'
 
-const TemplateSelect = (
-  {
-    onSelectTemplate,
-    onSetTemplate,
-    onClearTemplate,
-    userId,
-    userFirstName,
-    userLastName,
-  }:
-    {
-      onSelectTemplate: (arg0: string, arg1: string) => void,
-      onSetTemplate: (arg0: string, arg1: string) => void,
-      onClearTemplate: (arg0: string,) => void,
-      userId: string,
-      userFirstName: string | null,
-      userLastName: string | null
-    }
-) => {
+const TemplateSelect = ({
+  onSelectTemplate,
+  onSetTemplate,
+  onClearTemplate,
+  userId,
+  userFirstName,
+  userLastName,
+}: {
+  onSelectTemplate: (arg0: string, arg1: string) => void
+  onSetTemplate: (arg0: string, arg1: string) => void
+  onClearTemplate: (arg0: string) => void
+  userId: string
+  userFirstName: string | null
+  userLastName: string | null
+}) => {
+  const [template, setTemplate] = useState('')
+  const [isSet, setIsSet] = useState(false)
 
-  const [
-    template,
-    setTemplate,
-  ] = useState('')
-  const [
-    isSet,
-    setIsSet,
-  ] = useState(false)
-
-  const {
-    data: userPrograms, isLoading: userProgramsLoading,
-  } = api.userPrograms.getAll.useQuery()
-  const {
-    data: blocksData, isLoading: blocksLoading,
-  } = api.blocks.getAll.useQuery()
-  const { data: programsData, } = api.blocks.getAllPrograms.useQuery()
+  const { data: blocksData, isLoading: blocksLoading } =
+    api.blocks.getAll.useQuery()
+  const { data: programsData } = api.blocks.getAllPrograms.useQuery()
 
   const onSetLocalTemplate = (template: string) => {
     setTemplate(template)
@@ -59,50 +41,52 @@ const TemplateSelect = (
   }
 
   useEffect(() => {
-    const userProgram = userPrograms?.find((userProgram) => userProgram.userId === userId && userProgram.isProgramActive)
+    const userProgram = programsData?.find(
+      (userProgram) =>
+        userProgram.userIdOfProgram === userId &&
+        userProgram.isProgramActive === true,
+    )
+    console.log('userProgram', userProgram)
     if (!userProgram) {
       setIsSet(false)
       setTemplate('')
       return
     }
-    const templateName = programsData?.find((program) => program.id === userProgram.programId)?.name
+    const templateName = programsData?.find(
+      (program) => program.id === userProgram.id,
+    )?.name
     if (!templateName) return
     setIsSet(true)
     setTemplate(templateName)
-  }, [
-    userPrograms,
-    blocksData,
-    userId,
-    programsData,
-  ])
+  }, [blocksData, userId, programsData])
 
   const onSetTemplateWrapper = (template, userId) => {
     onSetTemplate(template, userId)
     setIsSet(false)
   }
 
-  if (userProgramsLoading || blocksLoading) return <div>loading</div>
+  if (blocksLoading) return <div>loading</div>
   const blocksTitle = blocksData?.map((block) => block.name)
 
   // if (userId === 'user_2Pg92dlfZkKBNFSB50z9GJJBJ2a') return null
 
   return (
-    <div className='m-2 flex flex-col md:flex-row gap-6 '>
-      <div className='text-lg w-64 font-semibold flex gap-2 items-center justify-between'>
-        <div
-          className='text-xl font-bold text-yellow-500'>
+    <div className='m-2 flex flex-col gap-6 md:flex-row '>
+      <div className='flex w-64 items-center justify-between gap-2 text-lg font-semibold'>
+        <div className='text-xl font-bold text-yellow-500'>
           {capitaliseString(userFirstName)} {capitaliseString(userLastName)}
         </div>
-          {isSet && (<CheckCircleIcon className='h-8 w-8 text-green-600' />)}
+        {isSet && <CheckCircleIcon className='h-8 w-8 text-green-600' />}
       </div>
 
-      <div className='flex sm:gap-2 justify-start md:items-center'>
-        <div className='text-sm md:text-base font-bold flex flex-col justify-center'>
-          <Listbox value={template} onChange={(e) => onSetLocalTemplate(e)}>
-            <div className='relative z-1'>
-              <Listbox.Button
-                className='relative w-60 h-10 border-b border-gray-600 hover:border-white cursor-default pl-3 pr-10 text-left shadow-md focus:outline-none '
-              >
+      <div className='flex justify-start sm:gap-2 md:items-center'>
+        <div className='flex flex-col justify-center text-sm font-bold md:text-base'>
+          <Listbox
+            value={template}
+            onChange={(e) => onSetLocalTemplate(e)}
+          >
+            <div className='z-1 relative'>
+              <Listbox.Button className='relative h-10 w-60 cursor-default border-b border-gray-600 pl-3 pr-10 text-left shadow-md hover:border-white focus:outline-none '>
                 <span className='block truncate'>{template}</span>
                 <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
                   <ChevronUpDownIcon
@@ -117,29 +101,30 @@ const TemplateSelect = (
                 leaveFrom='opacity-100'
                 leaveTo='opacity-0'
               >
-                <Listbox.Options className='absolute z-10 mt-1 max-h-120 w-full overflow-auto bg-black border-gray-600 border py-1 shadow-lg '>
+                <Listbox.Options className='max-h-120 absolute z-10 mt-1 w-full overflow-auto border border-gray-600 bg-black py-1 shadow-lg '>
                   {blocksTitle?.map((templateName, Idx) => (
                     <Listbox.Option
                       key={Idx}
-                      className={({ active, }) => `relative cursor-default select-none py-2 pl-8 pr-4 ${active ? 'bg-yellow-400 text-black' : 'text-gray-200'
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-8 pr-4 ${
+                          active ? 'bg-yellow-400 text-black' : 'text-gray-200'
                         }`
                       }
                       value={templateName}
                     >
-                      {({ selected, }) => (
+                      {({ selected }) => (
                         <>
-                          <span
-                            className={`block truncate`}
-                          >
+                          <span className={`block truncate`}>
                             {templateName}
                           </span>
-                          {selected
-                            ? (
-                              <span className='absolute inset-y-0 left-0 flex items-center pl-1'>
-                                <CheckIcon className='h-5 w-5' aria-hidden='true' />
-                              </span>
-                            )
-                            : null}
+                          {selected ? (
+                            <span className='absolute inset-y-0 left-0 flex items-center pl-1'>
+                              <CheckIcon
+                                className='h-5 w-5'
+                                aria-hidden='true'
+                              />
+                            </span>
+                          ) : null}
                         </>
                       )}
                     </Listbox.Option>
@@ -149,13 +134,13 @@ const TemplateSelect = (
             </div>
           </Listbox>
         </div>
-        <div className='flex gap-2 ml-4 justify-start items-end'>
+        <div className='ml-4 flex items-end justify-start gap-2'>
           <PencilSquareIcon
-            className='text-gray-400 hover:text-green-600 h-8 w-8'
+            className='h-8 w-8 text-gray-400 hover:text-green-600'
             onClick={() => onSetTemplateWrapper(template, userId)}
           />
           <XCircleIcon
-            className='text-gray-400 hover:text-red-600 h-8 w-8 col-span-1'
+            className='col-span-1 h-8 w-8 text-gray-400 hover:text-red-600'
             onClick={() => onClearTemplate(userId)}
           />
         </div>
