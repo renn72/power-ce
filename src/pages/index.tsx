f3f4ce285601bbdd0b56842bf73aeebf03ca04c9
renn72 <simplesam8856@gmail.com>
2023-10-15 11:13:33 +1100

    start user profile form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,60 +17,99 @@ import { getDate } from '~/utils/utils'
 import { Cog6ToothIcon } from '@heroicons/react/20/solid'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
+import {
+  useForm,
+  FormProvider,
+  useFieldArray,
+  Controller,
+} from 'react-hook-form'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { format, add } from 'date-fns'
+
 const Settings = () => {
   const { user } = useUser()
 
-  console.log(user)
-  const [goal1, setGoal1] = useState(
-    () => (user?.unsafeMetadata.goal1 as string) || '',
-  )
-  const [goal2, setGoal2] = useState(
-    () => (user?.unsafeMetadata.goal2 as string) || '',
-  )
-  const [goal3, setGoal3] = useState(
-    () => (user?.unsafeMetadata.goal3 as string) || '',
-  )
+  const formMethods = useForm()
+  const {
+    register,
+    reset,
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = formMethods
 
-  const onSetGoals = async () => {
-    if (!user) return null
-    await user.update({
-      unsafeMetadata: {
-        goal1: goal1,
-        goal2: goal2,
-        goal3: goal3,
-      },
-    })
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
+  const onError = (data: any) => {
+    console.log(data)
   }
 
   return (
     <>
       <div className='flex flex-col gap-4'>
         <div className='flex flex-col gap-1'>
-          <h3>Goals</h3>
-          <div className='flex flex-col gap-1'>
-            <div className='flex items-center gap-1'>
+          <FormProvider {...formMethods}>
+            <form
+              onSubmit={handleSubmit(onSubmit, onError)}
+              className='flex flex-col gap-4'
+            >
               <Input
-                value={goal1}
-                onChange={(e) => setGoal1(e.target.value)}
-                placeholder='Goal 1'
+                placeholder='Height'
+                {...register('height')}
+                defaultValue={''}
               />
-            </div>
-            <div className='flex items-center gap-1'>
               <Input
-                value={goal2}
-                onChange={(e) => setGoal2(e.target.value)}
-                placeholder='Goal 2'
+                placeholder='Weight'
+                {...register('weight')}
+                defaultValue={''}
               />
-            </div>
-            <div className='flex items-center gap-1'>
-              <Input
-                value={goal3}
-                onChange={(e) => setGoal3(e.target.value)}
-                placeholder='Goal 1'
+              <Controller
+                control={control}
+                name='dob'
+                defaultValue={''}
+                render={({ field: { onChange, value } }) => (
+                  <div className='flex items-end justify-between gap-2 md:justify-normal md:gap-6'>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          className={cn(
+                            'col-span-2 w-[190px] justify-start rounded-none border-0 border-b border-gray-600 px-1 text-left text-gray-200 hover:border-gray-200 md:w-[230px] md:px-2 ',
+                            !value && 'text-gray-600',
+                          )}
+                        >
+                          <CalendarIcon className='mr-2 h-4 w-4' />
+                          {value ? (
+                            format(value, 'PPP')
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className='z-10 w-auto bg-black py-3 text-gray-200 md:px-3'>
+                        <Calendar
+                          mode='single'
+                          selected={value}
+                          onSelect={(e) => {
+                            onChange(e)
+                          }}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                )}
               />
-            </div>
-            <Button onClick={onSetGoals}>Save</Button>
-          </div>
+            </form>
+          </FormProvider>
         </div>
       </div>
     </>
@@ -224,7 +263,7 @@ const Home: NextPage = () => {
             <h1 className='text-xl'>Profile</h1>
             <Cog6ToothIcon
               onClick={openModal}
-              className='hidden h-6 w-6 text-yellow-500'
+              className='h-6 w-6 cursor-pointer text-yellow-500'
             />
           </div>
           <div className='flex flex-col gap-0 text-sm text-gray-400'>
