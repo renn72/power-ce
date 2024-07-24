bfd8faeb59a30b6dadf6514b8e6728c66464885a
renn72 <simplesam8856@gmail.com>
2024-07-24 10:02:05 +1000

    add search

--- src/pages/templates2/exerciseDropper.tsx
+++ src/pages/templates2/exerciseDropper.tsx
@@ -2,144 +2,169 @@ import React, { useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { Droppable, Draggable } from '@hello-pangea/dnd'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { Input } from '@/components/ui/input'
 
 import { api } from '~/utils/api'
 import { cn } from '@/lib/utils'
 
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
 } from '@/components/ui/accordion'
 
 import ExerciseView from '~/components/exerciseView'
-import { X } from 'lucide-react'
+import { SearchIcon, X } from 'lucide-react'
 
 const ExerciseDropper = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const { data: session } = useSession()
-    const user = session?.user
-    const userId = user?.id || ''
-    const ctx = api.useUtils()
-    const { data: exerciseTemplates } =
-        api.templateBuilder.getAllYourExerciseTemplates.useQuery({
-            userId: userId,
-        })
+  const [isOpen, setIsOpen] = useState(false)
+  const { data: session } = useSession()
+  const [search, setSearch] = useState('')
+  const user = session?.user
+  const userId = user?.id || ''
+  const ctx = api.useUtils()
 
-    const { mutate: deleteExerciseTemplate } = api.exercise.delete.useMutation({
-        onSuccess: () => {
-            void ctx.templateBuilder.getAllYourExerciseTemplates.refetch()
-            void ctx.templateBuilder.getAllTemplateTitles.refetch()
-        },
-    })
-    return (
-        <div className=''>
-            <Accordion
-                type='single'
-                orientation='vertical'
-                collapsible
-                defaultValue={'0'}
-                className='mr-1 flex h-full flex-col items-center border-0'
+  const { data: exerciseTemplateaQuery } =
+  api.templateBuilder.getAllYourExerciseTemplates.useQuery({
+    userId: userId,
+  })
+
+  const exerciseTemplates = exerciseTemplateaQuery?.filter((t) => {
+    return t?.name?.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const { mutate: deleteExerciseTemplate } = api.exercise.delete.useMutation({
+    onSuccess: () => {
+      void ctx.templateBuilder.getAllYourExerciseTemplates.refetch()
+      void ctx.templateBuilder.getAllTemplateTitles.refetch()
+    },
+  })
+  return (
+    <div className=''>
+      <Accordion
+      type='single'
+      orientation='vertical'
+      collapsible
+      defaultValue={'0'}
+      className='mr-1 flex h-full flex-col items-center border-0'
+    >
+        <AccordionItem
+        className='border-0'
+        value={`0`}
+      >
+          <AccordionTrigger className='mt-10 flex flex-col pb-0'>
+            <div className='tracking-tigher flex w-full flex-col rounded-lg bg-gray-900 p-1 text-lg font-bold leading-snug'>
+              <div>Excercise</div>
+              <div>Templates</div>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className='w-80 rounded-md bg-gray-900 px-4 py-6'>
+            <div className='flex gap-2 items-center mb-4'>
+              <Input
+                placeholder='Search'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className='w-full bg-gray-900'
+              />
+              <SearchIcon
+                size={24}
+                className='cursor-pointer text-gray-400 hover:text-gray-200'
+                onClick={() => {
+                  setSearch('')
+                }}
+              />
+
+            </div>
+            <ScrollArea>
+              <Droppable
+              droppableId={`templates`}
+              renderClone={(provided, snapshot, rubric) => {
+                return (
+                  <div
+                    className={cn(
+                      snapshot.isClone
+                        ? 'bg-gray-600'
+                        : '',
+                      snapshot.isDragging
+                        ? 'bg-gray-700'
+                        : '',
+                      'rounded-md bg-gray-700',
+                    )}
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                      <ExerciseView
+                      exercise={
+                        exerciseTemplates?.[
+                        rubric.source.index
+                      ]
+                      }
+                      exerciseIdx={0}
+                      isAdmin={true}
+                    />
+                    </div>
+                )
+              }}
             >
-                <AccordionItem
-                    className='border-0'
-                    value={`0`}
-                >
-                    <AccordionTrigger className='mt-10 flex flex-col pb-0'>
-                        <div className='tracking-tigher flex w-full flex-col rounded-lg bg-gray-900 p-1 text-lg font-bold leading-snug'>
-                            <div>Excercise</div>
-                            <div>Templates</div>
-                        </div>
-                    </AccordionTrigger>
-                    <AccordionContent className='w-80 rounded-md bg-gray-900 px-4 py-6'>
-                        <ScrollArea>
-                            <Droppable
-                                droppableId={`templates`}
-                                renderClone={(provided, snapshot, rubric) => {
-                                    return (
-                                        <div
-                                            className={cn(
-                                                snapshot.isClone
-                                                    ? 'bg-gray-600'
-                                                    : '',
-                                                snapshot.isDragging
-                                                    ? 'bg-gray-700'
-                                                    : '',
-                                                'rounded-md bg-gray-700',
-                                            )}
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                        >
-                                            <ExerciseView
-                                                exercise={
-                                                    exerciseTemplates?.[
-                                                        rubric.source.index
-                                                    ]
-                                                }
-                                                exerciseIdx={0}
-                                                isAdmin={true}
-                                            />
-                                        </div>
-                                    )
-                                }}
-                            >
-                                {(provided, _snapshot) => (
-                                    <div
-                                        {...provided.droppableProps}
-                                        ref={provided.innerRef}
-                                        className='flex flex-col gap-2'
-                                    >
-                                        {exerciseTemplates?.map((t, i) => (
-                                            <Draggable
-                                                key={t.id}
-                                                draggableId={t.id}
-                                                index={i}
-                                            >
-                                                {(provided, snapshot) => (
-                                                    <div
-                                                        className={cn(
-                                                            snapshot.isClone
-                                                                ? 'bg-gray-600'
-                                                                : '',
-                                                            snapshot.isDragging
-                                                                ? 'bg-gray-700'
-                                                                : '',
-                                                            'rounded-md bg-gray-700 p-2 hover:bg-gray-700 relative',
-                                                        )}
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                    >
-                                                        <X
-                                                            size={20}
-                                                            className='absolute right-2 top-1 text-gray-400 hover:text-gray-200 cursor-pointer'
-                                                            onClick={() =>
-                                                                deleteExerciseTemplate({
-                                                                    id: t.id,
-                                                                })
-                                                            }
-                                                        />
-                                                        <ExerciseView
-                                                            exercise={t}
-                                                            exerciseIdx={i}
-                                                            isAdmin={true}
-                                                        />
-                                                    </div>
-                                                )}
-                                            </Draggable>
-                                        ))}
-                                        {provided.placeholder}
-                                    </div>
+                {(provided, _snapshot) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    className='flex flex-col gap-2'
+                  >
+                      {exerciseTemplates?.map((t, i) => (
+                        <Draggable
+                          key={t.id}
+                          draggableId={t.id}
+                          index={i}
+                        >
+                            {(provided, snapshot) => (
+                              <div
+                                className={cn(
+                                  snapshot.isClone
+                                    ? 'bg-gray-600'
+                                    : '',
+                                  snapshot.isDragging
+                                    ? 'bg-gray-700'
+                                    : '',
+                                  'relative rounded-md bg-gray-700 p-2 hover:bg-gray-700',
                                 )}
-                            </Droppable>
-                        </ScrollArea>
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
-        </div>
-    )
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                  <X
+                                  size={20}
+                                  className='absolute right-2 top-1 cursor-pointer text-gray-400 hover:text-gray-200'
+                                  onClick={() =>
+                                    deleteExerciseTemplate(
+                                      {
+                                        id: t.id,
+                                      },
+                                    )
+                                  }
+                                />
+                                  <ExerciseView
+                                  exercise={t}
+                                  exerciseIdx={i}
+                                  isAdmin={true}
+                                />
+                                </div>
+                            )}
+                          </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                )}
+              </Droppable>
+            </ScrollArea>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      </div>
+  )
 }
 
 export default ExerciseDropper
