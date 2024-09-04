80d6d5ab03b7eab0435fbd16d54f3c6c485fa5b3
renn72 <simplesam8856@gmail.com>
2024-09-04 10:39:20 +1000

    maxdur

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,7 +2,7 @@ import { createNextApiHandler } from '@trpc/server/adapters/next'
 import { env } from '~/env.mjs'
 import { appRouter } from '~/server/api/root'
 import { createTRPCContext } from '~/server/api/trpc'
-
+export const maxDuration = 40
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
