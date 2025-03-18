e4eaf1452f0fc345f066cff23c5e6d5a611a53c9
renn <renn@warner.systems>
2025-03-18 15:30:11 +1100

    shrink size

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -68,7 +68,7 @@ const Layout = (props: PropsWithChildren) => {
         ) : (
           <div className='flex h-full w-full grow flex-col items-center justify-center gap-6'>
             <input
-              className='rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20'
+              className='rounded-full bg-white/10 px-4 py-3 font-semibold text-white no-underline transition hover:bg-white/20'
               placeholder='Email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
